b2a175b0e62befa9fe8020cc409513404a766c52
Fernando Medina <fermmm@gmail.com>
2018-08-23 22:04:24 -0300

    Improved index.html template.

--- src/components/pages/HelloWorld/HelloWorld.tsx
+++ src/components/pages/HelloWorld/HelloWorld.tsx
@@ -5,10 +5,7 @@ interface IProps { }
 export class HelloWorld extends React.PureComponent<IProps> {
     public render(): JSX.Element {
         return (
-            <h1 style={{ textAlign: "center" }}>
-                Boilerplate works!... It's obvious already but you keep reading because
-                you want to know what else is here to read, but it finishes here.
-            </h1>
+            <h1 style={{ textAlign: "center" }}> Boilerplate is working </h1>
         );
     }
 }
