d99d14b1ba9a60265b337dc9b18b56ee90e960ef
Fernando Medina <fermmm@gmail.com>
2018-09-06 01:39:04 -0300

    Changed hello world message

--- src/components/pages/HelloWorld/HelloWorld.tsx
+++ src/components/pages/HelloWorld/HelloWorld.tsx
@@ -6,7 +6,9 @@ interface IProps { }
 export class HelloWorld extends React.PureComponent<IProps> {
     public render(): JSX.Element {
         return (
-            <h1 className={styles.helloWorld}> Boilerplate is working </h1>
+            <h1 className={styles.helloWorld}> 
+                This project is so beautiful at this point, take a picture of the code.
+            </h1>
         );
     }
 }
