5bce6f5bd67d10356d45ed6eff6400ffd054889f
Fernando Medina <fermmm@gmail.com>
2018-09-06 01:23:05 -0300

    Added scss example to example component

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 1) run `npm install` to install dependencies.
 2) Highly recomended: Install TSLint extension for VSCode or for your prefered text editor.
-3) Optional: This project is compatible with the chrome extension: "Redux DevTools", you can install it.
+3) Optional: This project is compatible with the chrome extension: "Redux DevTools", you can install it too.
 
 ----
 #### To start coding and testing
--- src/components/pages/HelloWorld/HelloWorld.tsx
+++ src/components/pages/HelloWorld/HelloWorld.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
+const styles: any = require("./HelloWorld.scss"); 
 
 interface IProps { }
 
 export class HelloWorld extends React.PureComponent<IProps> {
     public render(): JSX.Element {
         return (
-            <h1 style={{ textAlign: "center" }}> Boilerplate is working </h1>
+            <h1 className={styles.helloWorld}> Boilerplate is working </h1>
         );
     }
 }
