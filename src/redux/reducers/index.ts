437de9e4a47dfcc666a2bf01737b85e66120a2d4
Fernando Medina <fermmm@gmail.com>
2018-09-25 16:14:01 -0300

    Improved redux typing

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,8 +2,12 @@ import { AppReducer } from "./AppReducer";
 import { IAppState } from "./interfaces";
 import { IReduxAction } from "../interfaces";
 
-const rootReducer: { appState: (state?: IAppState, action?: IReduxAction) => IAppState } = {
+const rootReducer: IRootReducer = {
     appState: AppReducer,
 };
+
+export interface IRootReducer {
+    appState: (state?: IAppState, action?: IReduxAction) => IAppState;
+}
 
 export { rootReducer };
