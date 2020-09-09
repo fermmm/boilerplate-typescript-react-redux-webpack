f79d51f7a8b8585310c2cdb83fc4c7ea67706fc9
Fernando Medina <fermmm@gmail.com>
2020-09-09 00:28:48 -0300

    Improved example component syntax

--- src/components/pages/CompleteComponent/CompleteComponent.tsx
+++ src/components/pages/CompleteComponent/CompleteComponent.tsx
@@ -51,11 +51,11 @@ export const CompleteComponent: FC<CompleteComponentProps> = ({ history }) => {
          Star Wars open API:
          <br />
          <br />
-         {requestResponse.isLoading
-            ? 'Making a request to an example URL...'
-            : requestResponse.isError
-            ? requestResponse.errorMessage
-            : requestResponse.data?.results.map(r => <div key={r.episode_id}>{r.title}</div>)}
+         {requestResponse.isLoading && 'Making a request to an example URL...'}
+         {requestResponse.isError && requestResponse.errorMessage}
+         {requestResponse.data?.results?.map(r => (
+            <div key={r.episode_id}>{r.title}</div>
+         ))}
       </div>
    );
 };
